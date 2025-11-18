4b1c6f4bc9ef2427f45aeb4d090cabae7d9c40a1
Krishna Kashyap <krishna.kashyap50@gmail.com>
2025-11-18 14:33:43 -0500

    commit_multiple

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import "dotenv/config";
 import session from 'express-session';
+import morgan from "morgan";
 
 import Lab5 from './Lab5/index.js';
 import Hello from './Hello.js';
@@ -12,18 +13,37 @@ import CourseRoutes from "./Kambaz/Courses/routes.js";
 import EnrollmentRoutes from "./Kambaz/Enrollments/routes.js";
 import ModuleRoutes from "./Kambaz/Modules/routes.js";
 import AssignmentRoutes from "./Kambaz/Assignments/routes.js";
-import morgan from "morgan";
+
 const app = express();
 app.use(morgan("dev"));
-app.use(cors({
-   origin: process.env.CLIENT_URL || "http://localhost:3000",
-  credentials: true
-}));
+
+const allowedOrigins = [
+  process.env.CLIENT_URL,     
+  process.env.CLIENT_URL_2, 
+  "http://localhost:3000"     
+].filter(Boolean);
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("CORS blocked: " + origin));
+    },
+    credentials: true,
+  })
+);
+
 const sessionOptions = {
   secret: process.env.SESSION_SECRET || "kambaz",
   resave: false,
   saveUninitialized: false,
 };
+
 if (process.env.SERVER_ENV !== "development") {
   sessionOptions.proxy = true;
   sessionOptions.cookie = {
@@ -32,17 +52,18 @@ if (process.env.SERVER_ENV !== "development") {
     domain: process.env.SERVER_URL,
   };
 }
+
 app.use(session(sessionOptions));
-
-
 app.use(express.json());
 
-app.use(session({
-  secret: "secret",
-  resave: false,
-  saveUninitialized: false,
-  cookie: { sameSite: "lax" }
-}));
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { sameSite: "lax" },
+  })
+);
 
 UserRoutes(app, db);
 CourseRoutes(app, db);
